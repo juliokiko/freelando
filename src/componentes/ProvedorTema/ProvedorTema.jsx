78d1e5f9c88b8ba181fb965d868964abc8191299
juliokiko <juliocesar230398@hotmail.com>
2025-11-06 19:50:02 -0300

    Quase terminando...

--- src/componentes/ProvedorTema/ProvedorTema.jsx
+++ src/componentes/ProvedorTema/ProvedorTema.jsx
@@ -2,34 +2,35 @@ import { ThemeProvider } from "@emotion/react";
 
 const tema = {
   cores: {
-    branco: "",
-    atencao: "",
-    focos: "",
+    branco: "#FFFFFF",
+    atencao: "#FF0E0E",
+    focus: "#B009FF",
 
     primarias: {
       a: "#5754ed",
-      b: "",
-      c: "",
+      b: "#D93114",
+      c: "#168070",
     },
     secundarias: {
-      a: "#ebeaf9",
-      b: "",
-      c: "",
+      a: "#F8F8FD",
+      b: "#FDF8F8",
+      c: "#EBFCF9",
     },
     neutras: {
-      a: "",
-      b: "",
-      c: "",
+      a: "#373737",
+      b: "#E8E8E8",
+      c: "#F5F5F5",
       d: "",
     },
     dark: {
-      a: "",
-      b: "",
+      a: "#110EA0",
+      b: "#B61B00",
     },
   },
   espacamentos: {
     s: "16px",
     l: "32px",
+    xs: "8px",
   },
   fontFamily: "Montserrat, sans-serif",
 };
