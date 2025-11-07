bcb2247ab047c1a365c41a659004a812bf1c4dbc
juliokiko <juliocesar230398@hotmail.com>
2025-11-06 22:02:41 -0300

    Perfeito!

--- src/componentes/ListaSuspensa/ListaSuspensa.jsx
+++ src/componentes/ListaSuspensa/ListaSuspensa.jsx
@@ -79,6 +79,8 @@ export const ListaSuspensa = ({ titulo, opcoes }) => {
         setOpcaoFocada((focoAntigo) => {
           if (focoAntigo === null) return 0;
 
+          if (focoAntigo === opcoes.length - 1) return opcoes.length - 1;
+
           return (focoAntigo += 1);
         });
         break;
@@ -97,6 +99,13 @@ export const ListaSuspensa = ({ titulo, opcoes }) => {
         setOpcaoFocada(null);
         alternarVisibilidade(false);
         setOpcaoSelecionada(opcoes[opcaoFocada]);
+        break;
+
+      case "Escape":
+      case "Tab":
+        evento.preventDefault();
+        setOpcaoFocada(null);
+        alternarVisibilidade(false);
         break;
 
       default:
